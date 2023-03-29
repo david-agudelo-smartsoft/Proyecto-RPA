32f87fe00ed811bc31bd730b5d44cb86432bf28b
david-agudelo-smartsoft <david.agudelo@smartsoft.com.co>
2023-03-29 16:03:57 -0500

    Context API
    Se crea componente de autenticación.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home'
 import { Login } from './components/Login';
 import { Register } from './components/Register';
-
+import { AuthProvider } from './context/authContext'
 function App() {
   return (
     <div className="bg-slate-300 h-screen text-white flex">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
     </div>
-
   );
 }
 
