e7ad2c586d49d2726c90d8d262112b73f560448c
edinsonZapata <edinson.zapata@smartsoft.com.co>
2023-04-12 15:40:37 -0500

    info

--- src/components/table/AgentTable.js
+++ src/components/table/AgentTable.js
@@ -8,32 +8,16 @@ function AgentTable() {
             <Table striped bordered hover>
                 <thead>
                     <tr>
-                        <th>ID tareas</th>
-                        <th>Estados</th>
-                        <th>Paramatros</th>
-                        <th>Cliente</th>
-                        <th>Bot</th>
-                        <th>Agente</th>
-                        <th>Número de Intentos</th>
-                        <th>Tiempo</th>
-                        <th>Reiniciar tarea</th>
+                        <th>Nombre</th>
+                        <th>Estado</th>
+                        <th>Ultima conexión</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
-                        <td>Activo</td>
-                        <td>Parámetro 1</td>
-                        <td>Cliente A</td>
-                        <td>Bot 1</td>
-                        <td>Agente 1</td>
-                        <td>3</td>
-                        <td>00:12:35</td>
-                        <td>
-                            <button type="button" className="btn btn-primary">
-                                Reiniciar
-                            </button>
-                        </td>
+                        <td>Nombre usuario (Editable)</td>
+                        <td>Lista desplegable</td>
+                        <td>05/04/2023 03:00 am (Validación con colores)</td>
                     </tr>
                 </tbody>
             </Table>
