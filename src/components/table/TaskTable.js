a3a0f63f8ec6ef8df5e0d7ebf1e2618811b2179b
david-agudelo-smartsoft <david.agudelo@smartsoft.com.co>
2023-04-12 15:40:31 -0500

    Modal
    0002-DA
    Se crea modal para mostrar la info

--- src/components/table/TaskTable.js
+++ src/components/table/TaskTable.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TaskTable.css"
 import Table from "react-bootstrap/Table";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
 function TaskTable() {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     return (
         <div className='MainContainer'>
             <Table striped bordered hover>
@@ -21,22 +30,42 @@ function TaskTable() {
                 </thead>
                 <tbody>
                     <tr>
-                        <td>1</td>
-                        <td>Activo</td>
-                        <td>Parámetro 1</td>
-                        <td>Cliente A</td>
-                        <td>Bot 1</td>
+                        <td>20230000001871</td>
+                        <td>Nueva</td>
+                        <td className="text-center">
+                            <Button variant="primary" size='sm' onClick={handleShowModal}>
+                                Tipo de consulta
+                            </Button>
+                        </td>
+                        <td>David Agudelo</td>
+                        <td>Test</td>
                         <td>Agente 1</td>
-                        <td>3</td>
+                        <td className="text-center">3</td>
                         <td>00:12:35</td>
-                        <td>
-                            <button type="button" className="btn btn-primary">
-                                Reiniciar
+                        <td className="text-center">
+                            <button>
+                                <FontAwesomeIcon onClick={handleShowModal} className="rotate-icon" icon={faArrowsRotate} />
                             </button>
                         </td>
                     </tr>
                 </tbody>
             </Table>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Tipo de consulta / Log</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Esta es la información del parámetro 1.</p>
+                    <p>Esta es la información del parámetro 1.</p>
+                    <p>Esta es la información del parámetro 1.</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
